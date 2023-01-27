33e3dfc653171a4c8606ad8ea7c2f89177cfbcc8
Marcos Vinicius <marcao.vfo@gmail.com>
2023-01-27 19:15:55 -0400

    feat(chore): config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
